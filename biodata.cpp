44721c49a1cb5c3e57016475c5ceeba97718c491
Naufal Hilal <92293302+NaufallHilal@users.noreply.github.com>
2021-11-04 16:30:43 +0700

    Update biodata.cpp

--- biodata.cpp
+++ biodata.cpp
@@ -1,21 +1,30 @@
-#include<iostream>
-
+#include <iostream>
+#include <ncurses.h>
+#include <windows.h>
 using namespace std;
 
 int main(){
-	//input
-	string nama;
-	string sekolah;
-	string jk;
-	cout<<"Masukan Data Berikut :"<<endl;
-	cout<<"1.Nama          : ";
-	getline(cin,nama);
-	cout<<"2.Sekolah       : ";
-	getline(cin,sekolah);
-	cout<<"3.Jenis Kelamin : ";
-	getline(cin,jk);cout<<"."<<endl;
-	//output
-	cout<<"Data Siswa :"<<endl;
-	cout<<"Nama Anda Adalah = "<<nama<<endl<<"Sekolah di 	 = "<<sekolah<<endl<<"Jenis Kelamin 	 = "<<jk;
-	return 0;
+
+	string nama="Naufal Hilal";
+	string jeniskelamin="Laki Laki";
+	string jurusan="Ilmu Komputer";
+	int npm=2117051020,i;
+	
+	cout<<"\t     HELLO!\n";
+	refresh();Sleep(1000);
+	cout<<" Berikut Ini Adalah Biodata Saya :\n";
+	refresh();Sleep(1000);
+	for(i=0;i<=35;i++){
+		cout<<"=";
+		refresh();Sleep(0050);
+	}
+	cout<<"\n";
+	refresh();Sleep(1000);
+	cout<<" Nama\t\t: "<<nama<<endl;
+	refresh();Sleep(1000);
+	cout<<" Jenis Kelamin\t: "<<jeniskelamin<<endl;
+	refresh();Sleep(1000);
+	cout<<" Jurusan\t: "<<jurusan<<endl;
+	refresh();Sleep(1000);
+	cout<<" NPM\t\t: "<<npm<<endl;
 }
